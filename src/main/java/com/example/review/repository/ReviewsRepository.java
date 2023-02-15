db4d56e43457cfd2a20093c18935eca086370be7
M1urray <rknjonjo@gmail.com>
2023-02-15 15:16:43 +0300

    add database query methods

--- src/main/java/com/example/review/repository/ReviewsRepository.java
+++ src/main/java/com/example/review/repository/ReviewsRepository.java
@@ -3,7 +3,14 @@ package com.example.review.repository;
 import com.example.review.model.Reviews;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReviewsRepository extends JpaRepository<Reviews, UUID> {
+    List<Reviews> findAll();
+
+    List<Reviews> findByIsbnOrderByDateDesc(long isbn);
+
+    Optional<Reviews> findByreview_id(int id);
 }
